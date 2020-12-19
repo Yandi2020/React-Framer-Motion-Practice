a2b4825e4c3267e2e1e81a81649d2ff7cee9235f
Yandi2020 <wangyandi0406@gmail.com>
2020-12-19 14:04:51 +0100

    Updates

---
+++ precache-manifest.a8a3c279f807c968eb32f09e56244650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bfa8660c29d6f222addc03654de33d",
+    "url": "/React-Framer-Motion-Practice/index.html"
+  },
+  {
+    "revision": "ee06447c079be2b1fe83",
+    "url": "/React-Framer-Motion-Practice/static/css/main.db51ff1a.chunk.css"
+  },
+  {
+    "revision": "a6c92f9410f288921408",
+    "url": "/React-Framer-Motion-Practice/static/js/2.3fdb4e2c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/React-Framer-Motion-Practice/static/js/2.3fdb4e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee06447c079be2b1fe83",
+    "url": "/React-Framer-Motion-Practice/static/js/main.101ae170.chunk.js"
+  },
+  {
+    "revision": "ebfe701d786f9d7f1bbc",
+    "url": "/React-Framer-Motion-Practice/static/js/runtime-main.42ec8e43.js"
+  }
+]);
